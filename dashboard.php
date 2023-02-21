fb0ef2abee3a456b01189750a9810b658cb3ba43
DanielFransiscus <dafransi123@gmail.com>
2023-02-21 11:18:20 +0700

    ganti margin top

--- dashboard.php
+++ dashboard.php
@@ -62,7 +62,7 @@ GROUP BY nama ORDER BY qty DESC , id_barang ASC LIMIT 5");
       <main>
         <div class="container-fluid px-4">
           <!-- awal -->
-          <h1 class="mt-4 mb-4">Dashboard</h1>
+          <h1 class="mb-4">Dashboard</h1>
           <div class="row">
             <div class="col-xl-3 col-md-6">
               <div class="card bg-danger text-white mb-4">
